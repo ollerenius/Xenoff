7f7eb0e49de9a72984b17229e9ee4d2867946b25
Olle Renius <olle_9516@hotmail.com>
2018-01-02 13:05:47 +0100

    Finish Lecture 43 - Make enemies follow player
    The Enemy script now utilizes the AI Controller standard asset and a nav mesh agent to run towards the player when it is close enough. All changes where stored in the Enemy prefab.

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
+[RequireComponent(typeof(ThirdPersonCharacter))]
+[RequireComponent(typeof(AICharacterControl))]
 public class Enemy : MonoBehaviour {
 
 	[SerializeField]
 	float attackRange = 1f;
 	[SerializeField]
+	float attackRadius = 4f;
+	[SerializeField]
 	float maxHealthPoints = 100f;
 	float currentHealthPoints = 100f;
+	AICharacterControl aiCharacterControl;
+	GameObject player;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player");
+		aiCharacterControl = GetComponent<AICharacterControl>();
+	}
+
+	void Update() {
+		float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+		if (distanceToPlayer < attackRadius) {
+			aiCharacterControl.SetTarget(player.transform);
+		} else {
+			aiCharacterControl.SetTarget(transform);
+		}
+	}
 
 	public float AttackRange { get { return attackRange; } }
 
