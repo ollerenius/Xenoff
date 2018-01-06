2998c238c5a22b4b76405839d7bf7e5c4e8af511
Olle Renius <olle_9516@hotmail.com>
2018-01-06 16:16:21 +0100

    Add UI element that prints current combat state
    For debugging purposes, a text field was added to the game canvas that prints the current combat state.
    
    Similar functionality was used in EnemyTargetedMarker before; it is now removed from that script.

---
+++ Assets/Camera & UI/CombatStateUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Script used for writing the current combat state to the screen, for debugging purposes.
+/// Attatch it to a text field on a canvas.
+/// </summary>
+public class CombatStateUI : MonoBehaviour {
+
+	Text combatStateText = null;
+	
+	void Start () {
+		combatStateText = GetComponent<Text>();
+		combatStateText.text = CombatState.DEFAULT.ToString();
+		CombatController.instance.notifyCombatStateChangeObservers += OnCombatStateChange;
+	}
+
+	void OnCombatStateChange(CombatState state) {
+		combatStateText.text = state.ToString();
+	}
+}
--- Assets/Enemy/EnemyTargetetMarker.cs
+++ Assets/Enemy/EnemyTargetetMarker.cs
@@ -8,26 +8,8 @@ public class EnemyTargetetMarker : MonoBehaviour {
 
 	void Start () {
 		enemy = GetComponentInParent<Enemy>();
-		CombatController.instance.notifyCombatStateChangeObservers += OnCombatStateChange;
 		CombatController.instance.notifyOnTargetChangeListeners += OnTargetChange;
 		gameObject.SetActive(false);
-	}
-
-	/// <summary>
-	/// Listener method that is called whenever the current CombatState is changed in CombatController
-	/// </summary>
-	void OnCombatStateChange(CombatState state) {
-		print("Change combat state to " + state);
-		switch(state) {
-			case CombatState.COMBAT:
-				break;
-			case CombatState.DEFAULT:
-				break;
-			case CombatState.ENGAGED:
-				break;
-			default:
-				break;
-		}
 	}
 
 	void OnTargetChange(Enemy newTarget) {
